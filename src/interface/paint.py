99ed9dfffbf22478a9ba4069f13150cb4011f1dc
jcalandra <jcalandra001@enseirb-matmeca.fr>
2019-07-03 17:27:35 +0200

    cursor added

--- src/interface/paint.py
+++ src/interface/paint.py
@@ -4,17 +4,16 @@ from tkinter import *
 from tkinter.colorchooser import askcolor
 import PIL
 from PIL import Image, ImageDraw, ImageTk
+import Pict2audio_link
 print('PIL',Image.VERSION)
 
 ### PICT2AUDIO graphic interface
 ### Graphic interface to draw picutres assimilated to sounds
 
 #TODO :
-#       1) trouver comment faire une gomme pour l'affichage (blanc ok pour la sauvegarde)
-#       2) fonction clear l'image
-#       3) agencer les boutons correctement (grid ou pack)
-#       4) laisser une possibilite de sauvegarde avec ou sans grille.
-#       5) afficher un pointeur avec la taille du trait
+#       1) trouver comment faire une gomme pour l'affichage
+#       2) laisser une possibilite de sauvegarde avec ou sans grille.
+#       3) creer les boutons load_neural networks
 
 
 class Paint(object):
@@ -24,6 +23,7 @@ class Paint(object):
 
     def __init__(self):
         self.root = Tk()
+        self.root.title('Pict2Audio')
 
         ## --- title
         title = Label(self.root, text="Pict2Audio", font=("Helvetica", 24))
@@ -46,13 +46,15 @@ class Paint(object):
         self.choose_size_button = Scale(self.root, from_=1, to=30, orient=HORIZONTAL)
         self.choose_size_button.grid(row=3, column=4)
 
-        self.c = Canvas(self.root, bg='white', width=400, height=400)
+        self.c = Canvas(self.root, bg='white', cursor="cross", width=400, height=400)
         self.c.grid(row=4, columnspan=5)
-
-        self.base = PIL.ImageTk.PhotoImage(file = "../../data/base_quadrillage.png")
+        path_base = "../../data/base_quadrillage.png"
+        self.base = PIL.ImageTk.PhotoImage(file = path_base)
         self.c.create_image(400, 400, image = self.base, anchor = SE)
 
-        self.img = PIL.Image.new('RGB', (400, 400), 'white')
+        # self.img = PIL.Image.new('RGB', (400, 400), 'white')
+        self.img = Image.open(path_base)
+        self.rgb_img = self.img.convert('RGB')
         self.draw = ImageDraw.Draw(self.img)
 
         self.ent = Entry(self.root)
@@ -82,9 +84,14 @@ class Paint(object):
         self.activate_button(self.pen_button)
 
     def use_clear(self):
-        self.activate_button(self.clear_button)
+        self.c.create_image(400, 400, image = self.base, anchor = SE)
+        path_base = "../../data/base_quadrillage.png"
+        self.img = Image.open(path_base)
+        self.draw = ImageDraw.Draw(self.img)
+        self.activate_button(self.pen_button)
 
     def choose_color(self):
+        self.activate_button(self.color_button)
         self.eraser_on = False
         self.color = askcolor(color=self.color)[1]
         self.activate_button(self.pen_button)
@@ -100,6 +107,9 @@ class Paint(object):
         print(filename + " has been saved")
 
     def get_sound(self):
+        self.img.save("img2analyse.png")
+        Pict2audio_link.main()
+        # supprimer éventuellement l'image
         self.activate_button(self.sound_button)
 
     def activate_button(self, some_button, eraser_mode=False):
@@ -110,8 +120,10 @@ class Paint(object):
 
     def paint(self, event):
         self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
+        paint_color = self.color
         if self.old_x and self.old_y:
+            if self.eraser_on :
+                paint_color = "white"
             self.c.create_line(self.old_x, self.old_y, event.x, event.y,
                                width=self.line_width, fill=paint_color, capstyle=ROUND, smooth=TRUE, splinesteps=36)
             self.draw.line([(self.old_x, self.old_y), (event.x, event.y)], width=self.line_width, fill=paint_color, joint='curve')
