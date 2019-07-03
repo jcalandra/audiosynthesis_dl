716929896f68fb109540a3ae33e435768665035d
jcalandra <jcalandra001@enseirb-matmeca.fr>
2019-07-03 17:29:03 +0200

    modification of Pict2audio_link.py and audio2csv.py

--- src/interface/Pict2audio_link.py
+++ src/interface/Pict2audio_link.py
@@ -1,82 +1,88 @@
-print('[INFO] loading the librairies...')
-import librosa
-import cv2
-import numpy as np
-import csv
-import IPython
-import tensorflow as tf
-import keras
-from keras.models import load_model
-
-print('tensorflow:', tf.__version__)
-print('keras:', keras.__version__)
-
-NB_CARACTERISTICS = 3
-
-# 1) Load the Pict2Audio_multineural network
-
-print('[INFO] Loading Neural Networks...')
-model_pitch = load_model('model_pitch.h5')
-model_thick = load_model('model_thick.h5')
-model_color = load_model('model_color.h5')
-
-# 2) Load a tab or csv with all sound labellised
-print('[INFO] Loading the csv file...')
-csvfile = open('audioLib.csv', newline='')
-filereader = csv.reader(csvfile, delimiter=';')
-
-# 3) draw the picture OK
-#    - exec paint.py
-#    - save the picture in a precise folder with a precise name
-
-# in the interface, button to display the following algorithm
-
-# 1) take the picture drawn
-#    - load the specific picture with the special name
-
-print('[INFO] loading the picture...')
-img_tab = np.empty((0,400,400,3))
-img = cv2.imread('2analyse.png')
-img_tab = np.concatenate((img_tab,np.reshape(img,(1,400,400,3))),axis=0)
+def importing_libraries():
+    print('[INFO] loading the librairies...')
+    import librosa
+    import cv2
+    import numpy as np
+    import csv
+    import IPython
+    import tensorflow as tf
+    import keras
+    from keras.models import load_model
+    import audio2csv
+    print('tensorflow:', tf.__version__)
+    print('keras:', keras.__version__)
 
 def process_data(image):
-  fimg = image.astype('float32')
-  norm_img = fimg / 255
-  return norm_img
-
-x_img = process_data(img_tab)
-
-# 4) classify the picture
-#    - Put as entry of the neural network
-#    - 3 labels are obtained : pitch & thick & color
-print('[INFO] Prediction of the classes...')
-y_img = []
-y_img.append(model_pitch.predict_classes(x_img))
-y_img.append(model_thick.predict_classes(x_img))
-y_img.append(model_color.predict_classes(x_img))
-
-# 5) match the label of the picture with the labels of the sound dataset
-#    - search algorithm in a tab
-print('[INFO] Running algorithm...')
+    fimg = image.astype('float32')
+    norm_img = fimg / 255
+    return norm_img
 
 def match_algorithm(y, file):
     next(file)
     for row in file:
         i = 0
         while (y[i] == int(row[i+1])):
-            i++;
+            i = i+1;
             if (i == 3):
                 name = row[0]
                 print(name)
                 return(name)
     print("Can't find any sound from the Library that match with the given picture")
-    return('NULL')
+    return('ERROR')
 
-sound = match_algorithm(y_img, filereader)
 
-# 6) play the adequated sound
-#    - take the name in the tab,
-#    - look for the sound with this name in the lib
-#    - play the sound
+def main():
+    NB_CARACTERISTICS = 3
+    importing_libraries() #TODO : sortir de la fonction avec une machine qui compile tensorflow
 
-IPython.display.Audio((sound))
+    # 1) Load the Pict2Audio_multineural network
+    #TODO : éventuellement les charger séparemment
+    print('[INFO] Loading Neural Networks...')
+    model_pitch = load_model('model_pitch.h5')
+    model_thick = load_model('model_thick.h5')
+    model_color = load_model('model_color.h5')
+
+    # 2) Load a tab or csv with all sound labellised
+    print('[INFO] Loading the csv file...')
+    audio2csv.main() #TODO : idem, charger séparémment sur l'interface paint
+    csvfile = open('audioLib.csv', newline='')
+    filereader = csv.reader(csvfile, delimiter=';')
+
+    # 3) draw the picture OK
+    #    - exec paint.py
+    #    - save the picture in a precise folder with a precise name
+
+    # in the interface, button to display the following algorithm
+
+    # 1) take the picture drawn
+    #    - load the specific picture with the special name
+
+    print('[INFO] loading the picture...')
+    img_tab = np.empty((0,400,400,3))
+    img = cv2.imread('img2analyse.png')
+    img_tab = np.concatenate((img_tab,np.reshape(img,(1,400,400,3))),axis=0)
+
+    x_img = process_data(img_tab)
+
+    # 4) classify the picture
+    #    - Put as entry of the neural network
+    #    - 3 labels are obtained : pitch & thick & color
+    print('[INFO] Prediction of the classes...')
+    y_img = []
+    y_img.append(model_pitch.predict_classes(x_img))
+    y_img.append(model_thick.predict_classes(x_img))
+    y_img.append(model_color.predict_classes(x_img))
+
+    # 5) match the label of the picture with the labels of the sound dataset
+    #    - search algorithm in a tab
+    print('[INFO] Running algorithm...')
+    sound = match_algorithm(y_img, filereader)
+
+    # 6) play the adequated sound
+    #    - take the name in the tab,
+    #    - look for the sound with this name in the lib
+    #    - play the sound
+    if (sound != 'ERROR'):
+        IPython.display.Audio((sound))
+    else :
+        print('No sound can be displayed')
--- src/interface/audio2csv.py
+++ src/interface/audio2csv.py
@@ -1,100 +1,104 @@
-# Importing the python librairies
-print('[INFO] Importing Python libraries...')
-from __future__ import print_function
-import glob
-import numpy as np
-import librosa
-import tensorflow as tf
-import keras
-from keras.models import load_model
-print('tensorflow:', tf.__version__)
-print('keras:', keras.__version__)
+def importing_libraries():
+    # Importing the python librairies
+    print('[INFO] Importing Python libraries...')
+    from __future__ import print_function
+    import glob
+    import numpy as np
+    import librosa
+    import tensorflow as tf
+    import keras
+    from keras.models import load_model
+    print('tensorflow:', tf.__version__)
+    print('keras:', keras.__version__)
 
-# Some needed functions
-#TODO : factoriser le code
-SAMPLE_RATE = 16000
-INPUT_LENGTH = SAMPLE_RATE*4
-batch_size = 32
-NB_MELS = 320
+    # Some needed functions
+    #TODO : factoriser le code
+    SAMPLE_RATE = 16000
+    INPUT_LENGTH = SAMPLE_RATE*4
+    batch_size = 32
+    NB_MELS = 320
 
 def preprocess_audio_mel(audio, sample_rate=SAMPLE_RATE):
-  """ Generate a mel spectrogram in dB given an `audio` """
-  MAX = 10000
-  mel_spec = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels= NB_MELS)
-  mel_db = (librosa.power_to_db(mel_spec, ref=MAX) + 80)/80
-  #TODO :trouver une valeur adaptée pour MAX : on voudrait dans l'idéal tout entre 0 et 1.
-  return mel_db
+    """ Generate a mel spectrogram in dB given an `audio` """
+    MAX = 10000
+    mel_spec = librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels= NB_MELS)
+    mel_db = (librosa.power_to_db(mel_spec, ref=MAX) + 80)/80
+    #TODO :trouver une valeur adaptée pour MAX : on voudrait dans l'idéal tout entre 0 et 1.
+    return mel_db
 
 def load_audio_file(file_path, input_length=INPUT_LENGTH):
-  """ Extend/Reduce an audio `file_path` to the given `input_lengh` then generate its mel spectrogram """
-  data = librosa.core.load(file_path, sr=SAMPLE_RATE)[0] #, sr=16000
-  if len(data)>input_length:
-    max_offset = len(data)-input_length
-    offset = np.random.randint(max_offset)
-    data = data[offset:(input_length+offset)]
-  else:
-    if input_length > len(data):
-      max_offset = input_length - len(data)
-      offset = np.random.randint(max_offset)
+    """ Extend/Reduce an audio `file_path` to the given `input_lengh` then generate its mel spectrogram """
+    data = librosa.core.load(file_path, sr=SAMPLE_RATE)[0] #, sr=16000
+    if len(data)>input_length:
+        max_offset = len(data)-input_length
+        offset = np.random.randint(max_offset)
+        data = data[offset:(input_length+offset)]
     else:
-      offset = 0
-      data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
-      #note : maybe adding silence could be more accurate depending of the type of sound in input
-  data = preprocess_audio_mel(data)
-  return data
+        if input_length > len(data):
+            max_offset = input_length - len(data)
+            offset = np.random.randint(max_offset)
+        else:
+            offset = 0
+            data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+            #note : maybe adding silence could be more accurate depending of the type of sound in input
+    data = preprocess_audio_mel(data)
+    return data
 
 def generate_mels(snd_tab):
-  """ Put and reshape the mel spectrograms in a tab `snd_tab` """
-  x_tab = np.empty((len(snd_tab),NB_MELS,126, 1))
-  for i in range(len(snd_tab)) :
-    x_tab_i = np.reshape(load_audio_file(snd_tab[i]),(1,NB_MELS,126,1))
-    x_tab[i] = x_tab_i
-  return x_tab
+    """ Put and reshape the mel spectrograms in a tab `snd_tab` """
+    x_tab = np.empty((len(snd_tab),NB_MELS,126, 1))
+    for i in range(len(snd_tab)) :
+        x_tab_i = np.reshape(load_audio_file(snd_tab[i]),(1,NB_MELS,126,1))
+        x_tab[i] = x_tab_i
+    return x_tab
 
-# Importation of sounds libraries
-print('[INFO] Importing the sounds library...')
-path = "../"
-path_snd = "data/bdd_snd/"
-snd_train = glob.glob(path + path_snd + "snd_train/*.wav") #train_files
-snd_validation = glob.glob(path + path_snd + "snd_validation/*.wav") #validation_files
-snd_test = glob.glob(path + path_snd + "snd_test/*.wav") #test_files
+def main()
+    print('[INFO] Importing the python library...')
+    importing_libraries()
 
-snd = snd_train + snd_validation + snd_test
-NB_SND = len(snd)
+    # Importation of sounds libraries
+    path = "../"
+    path_snd = "data/bdd_snd/"
+    snd_train = glob.glob(path + path_snd + "snd_train/*.wav") #train_files
+    snd_validation = glob.glob(path + path_snd + "snd_validation/*.wav") #validation_files
+    snd_test = glob.glob(path + path_snd + "snd_test/*.wav") #test_files
 
-# Data processing for the neural network
-print('[INFO] generation of mel spectrograms...')
-x_tab = generate_mels(snd)
+    snd = snd_train + snd_validation + snd_test
+    NB_SND = len(snd)
 
-# Loading the needed neural networks
-print('[INFO] Loading Sounds Neural Networks...')
-path_models = "models/sound/"
-model_pitch = load_model(path + path_models + "model_pitchsnd.h5")
-model_vol = load_model(path + path_models + "model_thicksnd.h5")
-model_tone = load_model(path + path_models + "model_colorsnd.h5")
+    # Data processing for the neural network
+    print('[INFO] generation of mel spectrograms...')
+    x_tab = generate_mels(snd)
 
-# Creation of the CSV file
-print('[INFO] Creation of the CSV file...')
+    # Loading the needed neural networks
+    print('[INFO] Loading Sounds Neural Networks...')
+    path_models = "models/sound/"
+    model_pitch = load_model(path + path_models + "model_pitchsnd.h5")
+    model_vol = load_model(path + path_models + "model_thicksnd.h5")
+    model_tone = load_model(path + path_models + "model_colorsnd.h5")
 
-headers = [
-     u'Audio_names',
-     u'label_pitch',
-     u'label_vol',
-     u'label_tone'
-]
+    # Creation of the CSV file
+    print('[INFO] Creation of the CSV file...')
 
-# remplir avec les labels correspondants
-labels_pitch = model_pitch.predict_classes(x_tab)
-labels_vol = model_vol.predict_classes(x_tab)
-labels_tone = model_tone.predict_classes(x_tab)
+    headers = [
+         u'Audio_names',
+         u'label_pitch',
+         u'label_vol',
+         u'label_tone'
+    ]
 
-# modifier label
-labels = [[snd[i],str(labels_pitch[i]), str(labels_vol[i]), str(labels_tone[i])] for i in range(NB_SND)]
+    # remplir avec les labels correspondants
+    labels_pitch = model_pitch.predict_classes(x_tab)
+    labels_vol = model_vol.predict_classes(x_tab)
+    labels_tone = model_tone.predict_classes(x_tab)
 
-f = open('audioLib.csv', 'w')
-lineHeader = ";".join(headers) + "\n"
-f.write(lineHeader)
-for label in labels:
-     line = ";".join(label) + "\n"
-     f.write(line)
-f.close()
+    # modifier label
+    labels = [[snd[i],str(labels_pitch[i]), str(labels_vol[i]), str(labels_tone[i])] for i in range(NB_SND)]
+
+    f = open('audioLib.csv', 'w')
+    lineHeader = ";".join(headers) + "\n"
+    f.write(lineHeader)
+    for label in labels:
+         line = ";".join(label) + "\n"
+         f.write(line)
+    f.close()
